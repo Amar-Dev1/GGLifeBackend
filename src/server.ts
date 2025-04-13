e611fe0a5d9ce4467c756fb8038d04917621fdae
Amar <3marofficial249@gmail.com>
2025-04-13 07:55:22 +0200

    finshed email verification

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,6 @@
-import dotenv from "dotenv";
 import app from "./app";
 
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient()
-
-dotenv.config();
-
 const PORT = process.env.PORT || 3000;
-
-app.delete('/dd', async (req, res)=>{
-await prisma.user.deleteMany()
-res.send('deleted')
-})
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
